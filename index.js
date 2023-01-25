95fac67060574846a5745811d028ab73f344f441
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-25 22:33:40 +0000

    update index.js to ES6 import module syntax

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+import { Manager } from './lib/Manager.js';
+import { Engineer } from './lib/Engineer.js';
+import { Intern } from './lib/Intern.js';
+
+import inquirer from 'inquirer';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+import { render } from './src/page-template.js';
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
