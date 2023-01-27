d1470aa956a43149687dcd50996f0e92101f59c6
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-27 18:31:26 +0000

    Refactor Questions class into separate file

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
+// external modules
 import inquirer from 'inquirer';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 
+// internal modules
 import Manager from './lib/Manager.js';
 import Engineer from './lib/Engineer.js';
 import Intern from './lib/Intern.js';
 import { render } from './src/page-template.js';
+import Questions from './lib/Questions.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-class Questions {
-    constructor(prompts = []) {
-        this.prompts = prompts;
-    }
-    addQuestion(name, message, validate) {
-        this.prompts.push({ type: 'input', name: name, message: message, validate: validate });
-        return this.this(); // this
-    }
-    addMenu(choices) {
-        this.prompts.push({ type: 'list', name: 'menu', message: 'Menu', choices: choices });
-        return this.this(); // this
-    }
-    this() { return this; }
-    value() { return this.prompts; }
-}
 
 function makeQuestions(role = 'Manager') {
 
