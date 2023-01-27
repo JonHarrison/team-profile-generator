17b58236e8877c34a87e6df04e4cf04c2a305b96
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-27 16:03:47 +0000

    re-arrange imports

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import Manager from './lib/Manager.js';
-import Engineer from './lib/Engineer.js';
-import Intern from './lib/Intern.js';
-
 import inquirer from 'inquirer';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
+
+import Manager from './lib/Manager.js';
+import Engineer from './lib/Engineer.js';
+import Intern from './lib/Intern.js';
+import { render } from './src/page-template.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,7 +14,6 @@ const __dirname = path.dirname(__filename);
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-import { render } from './src/page-template.js';
 
 let employees = [];
 
