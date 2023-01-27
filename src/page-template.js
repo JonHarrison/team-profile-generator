a017315b7390b0bd8c1eae1cd001c9ab4ff00e1d
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-27 12:51:20 +0000

    embed stylesheet within generated html

--- src/page-template.js
+++ src/page-template.js
@@ -93,7 +93,20 @@ const render = (team) => {
     <title>My Team</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <style>
+	.jumbotron {
+		background-color: rgb(232,71,86);
+		color: white;
+	}
+	.card {
+		margin: 10px;
+		background-color: rgb(246,247,249);
+	}
+	.card-header {
+		background-color: rgb(0,119,247);
+		color: white;
+	}
+	</style>
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 
